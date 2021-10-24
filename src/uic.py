db47f5ee1f89390d25bc10b8f3e547496caca0d5
Kim, Hyeonsung <qwqwhsnote@gm.gist.ac.kr>
2021-10-24 13:05:22 +0900

    Update uic.py

--- src/uic.py
+++ src/uic.py
@@ -94,14 +94,241 @@ class LED:
             self.ori = np.array([math.sin(self.phi)*math.cos(self.theta), math.sin(self.phi)*math.sin(self.theta), math.cos(self.phi)])
 
     @staticmethod
-    def cal_intensity( I, s, location, theta, phi, target):
+    def cal_intensity(I, s, location, theta, phi, target):
         d= target - location
         ori = np.array([math.sin(phi)*math.cos(theta), math.sin(phi)*math.sin(theta), math.cos(phi)])
         r2 = np.dot(d,d)
         dsize = math.sqrt(r2)
         cos = np.dot(d,ori)/dsize
 
-        return I/r2 * math.pow(cos,s)
+        return I/r2 * math.pow(cos,s) 
+
+#===================================================
+# Extended Sparrow criterion methods 
+# based on paper: Ivan Moreno, Maximino Avendaño-Alejo, and Rumen I. Tzonchev, "Designing light-emitting diode arrays for uniform near-field irradiance," Appl. Opt. 45, 2265-2272 (2006)
+# This methods does not use LED class only calculate x,y array for each LED for linear and rectangular array. 
+#===================================================
+
+class ESC:
+    def __init__(self):
+        pass
+    @classmethod
+    def esc_coefficient(cls, m,N,M=1,shape="L", approx = False):
+        # Varaible check 
+        #---------------------------------------------
+        result = 0
+        # Calculation functions
+        def esc_linear(x,N):
+            result = 0
+            for i in range(1,N+1):
+                result += (1-(m+3)*(N+1-2*i)**2 * (x**2)/4)*((N+1-2*i)**2 * (x**2)/4 +1)**(-(m+6)/2)
+            return result
+
+        def esc_rectangular(x, N, M):
+            result = 0
+            for i in range(1, N+1):
+                for k in range(1, M+1):
+                    result +=(1-((m+3)*(N+1-2*i)**2-(M+1-2*k)**2)*(x**2/4))*(((N+1-2*i)**2 + (M+1-2*j)**2)*(x**2/4)+1)**(-(m+6)/2)
+        # "L" : linear case
+        # "R" : Rectangular case
+        if shape == "L":
+            if approx == True and N>4 and m >30: #approximation coefficient, fast
+                result = math.sqrt(3.2773/m+4.2539)
+            else:
+                if N%2 ==0: # even LED
+                    if N ==2: # n=2 case is simple
+                        result = math.sqrt(4/(m+3))
+                    else:
+                        r = op.root_scalar(lambda x : esc_linear(x,N), bracket=[0,1], method = "brentq")
+                        result = r.root
+                else :
+                    r = op.minimize_scalar(lambda x: esc_linear(x,N), bounds=(0,1), method="bounded")
+                    result = r.x
+        elif shape == "R":
+            if approx == True and (N>4 and M>4) and m >30:
+                result = math.sqrt(1.2125/(m-3.349))
+            else:
+                if N == M and N==2:
+                    result = math.sqrt(4/(m+2))
+                else:
+                    try :
+                        r = op.root_scalar(lambda x: esc_rectangular(x,N,M), bracket=[0,1],method="brentq")
+                        if r.converged == False:
+                            r = op.minimize_scalar(lambda x: esc_rectangular(x,l,n,m), bounds=(0,1), method="bounded")
+                            result = r.x
+                        else:
+                            result = r.root
+                    except:
+                        r = op.minimize_scalar(lambda x: esc_rectangular(x,N,M), bounds=(0,1), method="bounded")
+                        result = r.x
+        return result
+    @classmethod
+    def esc_array(cls, m,h,N,M=1,shape="L",approx=False,half=True):
+        if shape == "L":
+            d = h * cls.esc_coefficient(m,N,M,shape=shape, approx = approx)
+            if half:
+                if N%2 :
+                    array = np.fromfunction(lambda i,j: (j) *d, (1,int((N+1)/2)), dtype =int)
+                else :
+                    array = np.fromfunction(lambda i,j : (1/2 +j)*d, (1,int(N/2)), dtype=int)
+            else :
+                array = np.fromfunction(lambda i,j: (-(N-1)/2+j)*d, (1,N), dtype=int)
+            return [(N,1),d, (d*(N-1),0), array ]
+        elif shape == "R":
+            raise ValueError()
+        else:
+            raise ValueError()
+
+
+#-------------------------------------------------------------
+
+
+#===================================================
+# Boundary enhancement method by Hyeon.
+# This method does not use LED class only calculates x,y array for each LED for linear and rectangular array. 
+#===================================================
+
+class bc_utils:
+    def __init__(self):
+        pass
+
+    def __intensity(x,t,phi, i, h ,s):
+        if math.isclose(phi,0,rel_tol=np.finfo(float).eps):
+            return h**s * i/(h**2 + (x-t)**2)**(s/2+1)
+        else:
+            sphi = math.copysign(1,phi)
+            l = sphi*(x-h/math.tan(phi))
+            sphi = math.copysign(1,phi)
+            if sphi*t< l:
+                messge = "Check phi, x, t\n sgn(phi) t > sgn(phi)*(x-h cot(phi))\n x:{}, t:{}, phi:{}".format(x,t,phi)
+                raise ValueError(messge)
+            r2 = (h**2 +(x-t)**2)**(s/2 +1)
+            ang = (h * math.cos(phi) + (t-x)*math.sin(phi))**s
+            return i *ang/r2
+        
+    @classmethod
+    def ic(cls,x,h,s,I,phi=0):
+        return (2*cls. __intensity(x,0,phi,I,h,s))
+    @classmethod
+    def ib(cls,x,h,s,w,I,phi=0):
+        return (cls.__intensity(-x,w/2,-phi,I,h,s)+cls.__intensity(x,w/2,phi,I,h,s))
+    @classmethod
+    def Di(cls, x,h,s,w,I,phi=0):
+        return cls.ic(x,h,s,I,phi) - cls.ib(x,h,s,w,I,phi)
+    @classmethod
+    def D(cls, alpha, d, s):
+        return 1/(1+ (0.5*alpha+d)**2)**(s/2+1) + 1/(1+ (0.5*alpha-d)**2)**(s/2+1) - 2/(1+d**2)**(s/2+1)
+    @classmethod
+    def find_de(cls, alpha, s, approx=False):
+        if approx:
+            return 0.25*alpha +  math.sqrt(2**(2/(s+2)) -1)/6 
+        else:
+            r = op.root_scalar(lambda x: cls.D(alpha,x, s), bracket=[0, alpha/2], method="brentq")
+            return r.root
+    @classmethod
+    def find_xe(cls, h, w, s, approx=False):
+        alpha = w/h
+        d = cls.find_de(alpha, s, approx=approx)
+        return h* d
+    @classmethod
+    def find_dm(cls, alpha, s, de, approx=False):
+        if approx:
+            return  math.sqrt(2**(2/(s+2)) -1)
+        else:
+            r = op.root_scalar(lambda x: cls.D(alpha, x, s), bracket = [0,de], method="brentq")
+            return r.root
+    @classmethod
+    def find_xm(cls, h, w, s, xe, approx=False):
+        alpha = w/h
+        de = xe/h
+        d = cls.find_dm(alpha, s, de, approx=approx)
+        return h*d
+    @classmethod
+    def find_corresponding_points(cls, arr, xe, xm, h, w, s, append=True):
+        if not hasattr(arr, "__len__"):
+            arr = [arr]
+
+        def core(x, xe, xm, h, w, s):
+            if x<0 or x>w/2:
+                raise ValueError("Argument 'x': must be in range [{},{}] \n x value= {}".format(0,w/2,x, x>0.0))
+            elif math.isclose(x,xe,rel_tol=np.finfo(float).eps):
+                return xe
+            elif math.isclose(x,w/2, rel_tol=np.finfo(float).eps):
+                return xm
+            elif  x<xm:
+                return w/2
+            elif xm < x and x < xe:
+                # find the xc in [xs,w/2]'
+                L = cls.Di(x,h,s,w,1)
+                r=op.root_scalar(lambda xc: cls.Di(xc, h,s,w,1) + L , bracket=[xe,w/2], method="brentq")
+                return r.root
+            elif xe< x and x <w/2:
+                # find the xc in [xm,xs]
+                L = cls.Di(x,h,s,w,1)
+                r=op.root_scalar(lambda xc: cls.Di(xc, h,s,w,1) + L , bracket=[xm,xe], method="brentq")
+                return r.root
+        # check arr is array like object
+        output = []
+        for x in arr:
+            output.append(core(x, xe, xm, h, w, s))
+
+
+        if append:
+            #append to given array
+            return np.array(arr.append(output))
+        else:
+            # only return ouput results
+            return np.array(output)
+
+
+def H_matrix(m,w1,w2,h, shape="L", I0=1):
+    def esc_linear(m,h,w, xe ,xm):
+        d_2 = math.sqrt(1/(m+3))*h
+        d_3 = math.sqrt(3/(m+3))*h
+
+        if d_2 < xm and d_3 < xm:
+            raise ValueError("check:\n d_2 = {d_2}, xm = {xm}\n d_3 = {d_3}, xm = {xm}")
+            
+        n =2
+        L  = esc_array(m,h,n,M=1,shape="L",approx=False,half=True)
+        Lw = L
+        Lw = L
+        Lw = L
+        while Lw[2][0]/2 < xe:
+            print("{}{}".format(Lw[1]/2,xe))
+            L = Lw
+            n += 1
+            Lw = esc_array(m,h,n,M=1,shape="L",approx=False,half=True)
+        return L[3]
+
+
+    xex = xe(h,w1,m)
+    xmx = xm(h,w2,m,xex)
+    xarr = esc_linear(m,h,w1, xex, xmx)
+    xarray = find_corres_BC(xarr, xex,xmx,h,w1,m)
+    
+    if shape == "R":
+        xey = xe(h,w1,m)
+        xmy = xm(h,w2,m,xey)
+        yarr = esc_linear(m,h,w2, xey, xmy)
+        yarray = find_corres_BC(yarr, xey,xmy,h,w2,m)
+        return LEDmatrix(m,I0,xarray,yarray)
+
+    return LEDmatrix(m,I0,xarray,np.array([[0]]))
+   
+
+'''
+def generate_LEDmatrix(m,h,N,M=1, method="hyeon"):
+
+    if method == "hyeon":
+        pass
+    elif method == "morenaL":
+        pass
+    elif method == "morenaR"
+
+    return LEDmatrix
+'''
+
 
 class LEDmatrix:
     def __init__(self, m, I0,  xarray, yarray=np.array([[0]])):
@@ -202,261 +429,4 @@ class LEDmatrix:
     @classmethod
     def esc_coefficient(cls, m, N, M=1, shap="L", approx=False):
     '''
-        
-
-#===================================================
-# Extended Sparrow criterion methods 
-# based on paper: Ivan Moreno, Maximino Avendaño-Alejo, and Rumen I. Tzonchev, "Designing light-emitting diode arrays for uniform near-field irradiance," Appl. Opt. 45, 2265-2272 (2006)
-# This methods does not use LED class only calculate x,y array for each LED for linear and rectangular array. 
-#===================================================
-
-def esc_coefficient(m,N,M=1,shape="L", approx = False):
-    # Varaible check 
-    #---------------------------------------------
-    result = 0
-    # Calculation functions
-    def esc_linear(x,N):
-        result = 0
-        for i in range(1,N+1):
-            result += (1-(m+3)*(N+1-2*i)**2 * (x**2)/4)*((N+1-2*i)**2 * (x**2)/4 +1)**(-(m+6)/2)
-        return result
-
-    def esc_rectangular(x, N, M):
-        result = 0
-        for i in range(1, N+1):
-            for k in range(1, M+1):
-                result +=(1-((m+3)*(N+1-2*i)**2-(M+1-2*k)**2)*(x**2/4))*(((N+1-2*i)**2 + (M+1-2*j)**2)*(x**2/4)+1)**(-(m+6)/2)
-    # "L" : linear case
-    # "R" : Rectangular case
-    if shape == "L":
-        if approx == True and N>4 and m >30: #approximation coefficient, fast
-            result = math.sqrt(3.2773/m+4.2539)
-        else:
-            if N%2 ==0: # even LED
-                if N ==2: # n=2 case is simple
-                    result = math.sqrt(4/(m+3))
-                else:
-                    r = op.root_scalar(lambda x : esc_linear(x,N), bracket=[0,1], method = "brentq")
-                    result = r.root
-            else :
-                r = op.minimize_scalar(lambda x: esc_linear(x,N), bounds=(0,1), method="bounded")
-                result = r.x
-    elif shape == "R":
-        if approx == True and (N>4 and M>4) and m >30:
-            result = math.sqrt(1.2125/(m-3.349))
-        else:
-            if N == M and N==2:
-                result = math.sqrt(4/(m+2))
-            else:
-                try :
-                    r = op.root_scalar(lambda x: esc_rectangular(x,N,M), bracket=[0,1],method="brentq")
-                    if r.converged == False:
-                        r = op.minimize_scalar(lambda x: esc_rectangular(x,l,n,m), bounds=(0,1), method="bounded")
-                        result = r.x
-                    else:
-                        result = r.root
-                except:
-                    r = op.minimize_scalar(lambda x: esc_rectangular(x,N,M), bounds=(0,1), method="bounded")
-                    result = r.x
-    return result
-
-'''
-def esc_array(m,h,N,M=1,shape="L",approx=False,half=True):
-    
-    # Varaible check 
-
-
-    #---------------------------------------------    
-
-    if shape == "L":
-        d = h * esc_coefficient(m,N,M,shape=shape, approx = approx)
-        if half:
-            if N%2 :
-                array = np.fromfunction(lambda i,j: (j) *d, (1,int((N+1)/2)), dtype =int)
-            else :
-                array = np.fromfunction(lambda i,j : (1/2 +j)*d, (1,int(N/2)), dtype=int)
-        else :
-            array = np.fromfunction(lambda i,j: (-(N-1)/2+j)*d, (1,N), dtype=int)
-        
-        return [(N,1),d, (d*(N-1),0), array ]
-    elif shape == "R":
-        raise ValueError()
-        pass
-'''
-
-#-------------------------------------------------------------
-
-
-#===================================================
-# Boundary enhancement method by Hyeon.
-# This method does not use LED class only calculates x,y array for each LED for linear and rectangular array. 
-#===================================================
-
-class bc_utils:
-    def __init__(self):
-        pass
-
-    def __intensity(x,t,phi, i, h ,s):
-        if math.isclose(phi,0,rel_tol=np.finfo(float).eps):
-            return h**s * i/(h**2 + (x-t)**2)**(s/2+1)
-        else:
-            sphi = math.copysign(1,phi)
-            l = sphi*(x-h/math.tan(phi))
-            sphi = math.copysign(1,phi)
-            if sphi*t< l:
-                messge = "Check phi, x, t\n sgn(phi) t > sgn(phi)*(x-h cot(phi))\n x:{}, t:{}, phi:{}".format(x,t,phi)
-                raise ValueError(messge)
-            r2 = (h**2 +(x-t)**2)**(s/2 +1)
-            ang = (h * math.cos(phi) + (t-x)*math.sin(phi))**s
-            return i *ang/r2
-        
-    @classmethod
-    def ic(cls,x,h,s,I,phi=0):
-        return (2*cls. __intensity(x,0,phi,I,h,s))
-    @classmethod
-    def ib(cls,x,h,s,w,I,phi=0):
-        return (cls.__intensity(-x,w/2,-phi,I,h,s)+cls.__intensity(x,w/2,phi,I,h,s))
-    @classmethod
-    def Di(cls, x,h,s,w,I,phi=0):
-        return cls.ic(x,h,s,I,phi) - cls.ib(x,h,s,w,I,phi)
-    @classmethod
-    def D(cls, alpha, d, s):
-        return 1/(1+ (0.5*alpha+d)**2)**(s/2+1) + 1/(1+ (0.5*alpha-d)**2)**(s/2+1) - 2/(1+d**2)**(s/2+1)
-    @classmethod
-    def find_de(cls, alpha, s, approx=False):
-        if approx:
-            return 0.25*alpha +  math.sqrt(2**(2/(s+2)) -1)/6 
-        else:
-            r = op.root_scalar(lambda x: cls.D(alpha,x, s), bracket=[0, alpha/2], method="brentq")
-            return r.root
-    @classmethod
-    def find_xe(cls, h, w, s, approx=False):
-        alpha = w/h
-        d = cls.find_de(alpha, s, approx=approx)
-        return h* d
-    @classmethod
-    def find_dm(cls, alpha, s, de, approx=False):
-        if approx:
-            return  math.sqrt(2**(2/(s+2)) -1)
-        else:
-            r = op.root_scalar(lambda x: cls.D(alpha, x, s), bracket = [0,de], method="brentq")
-            return r.root
-    @classmethod
-    def find_xm(cls, h, w, s, xe, approx=False):
-        alpha = w/h
-        de = xe/h
-        d = cls.find_dm(alpha, s, de, approx=approx)
-        return h*d
-    @classmethod
-    def find_corresponding_point(cls, arr, xe, xm, h, w, s, append=True):
-
-        def core(x, xe, xm, h, w, s):
-            if x<0 or x>w/2:
-                raise ValueError("Argument 'x': must be in range [{},{}] \n x value= {}".format(0,w/2,x, x>0.0))
-            elif math.isclose(x,xe,rel_tol=np.finfo(float).eps):
-                return xe
-            elif math.isclose(x,w/2, rel_tol=np.finfo(float).eps):
-                return xm
-            elif math.isclose(x,0,rel_tol=np.finfo(float).eps):
-                return w/2
-            elif xm < x and x < xe:
-                # find the xc in [xs,w/2]'
-                L = Ic(w/2,h,m,1,0)-Ib(w/2,h,m,w,1,0)
-                r=op.root_scalar(lambda x: Ib(w/2,h,m,w,1,0)-Ic(w/2,h,m,1,0) -L, bracket=[xe,w/2], method="brentq")
-                return r.root
-            elif  x<xm:
-                return w/2
-            elif xe< x and x <w/2:
-                # find the xc in [xm,xs]
-
-                L = Ib(w/2,h,m,w,1,0)-Ic(w/2,h,m,1,0)
-                r=op.root_scalar(lambda x: Ic(w/2,h,m,1,0)-Ib(w/2,h,m,w,1,0) -L, bracket=[xm,xe], method="brentq")
-                return r.root
-        # check arr is array like object
-        if not hasattr(arr, "__len__"):
-
-        if append:
-            pass
-        else:
-            pass
-
-def find_corres_BC(arr,xe,xm,h,w,m, append=True):
-    def bc_find(x,xe,xm,h,w,m):
-        if x<0 or x>w/2:
-            raise ValueError("Argument 'x': must be in range [{},{}] \n x value= {}".format(0,w/2,x, x>0.0))
-        elif math.isclose(x,xe,rel_tol=np.finfo(float).eps):
-            return xe
-        elif math.isclose(x,w/2, rel_tol=np.finfo(float).eps):
-            return xm
-        elif math.isclose(x,0,rel_tol=np.finfo(float).eps):
-            return w/2
-        elif xm < x and x < xe:
-            # find the xc in [xs,w/2]'
-            L = Ic(w/2,h,m,1,0)-Ib(w/2,h,m,w,1,0)
-            r=op.root_scalar(lambda x: Ib(w/2,h,m,w,1,0)-Ic(w/2,h,m,1,0) -L, bracket=[xe,w/2], method="brentq")
-            return r.root
-        elif  x<xm:
-            return w/2
-        elif xe< x and x <w/2:
-            # find the xc in [xm,xs]
-
-            L = Ib(w/2,h,m,w,1,0)-Ic(w/2,h,m,1,0)
-            r=op.root_scalar(lambda x: Ic(w/2,h,m,1,0)-Ib(w/2,h,m,w,1,0) -L, bracket=[xm,xe], method="brentq")
-            return r.root
-    
-    correspoints = []
-    for x in arr[0]:
-        correspoints.append(bc_find(x,xe,xm,h,w,m))
-    if append:
-        return np.array([np.append(arr[0], np.array(correspoints))])
-    else:
-        return np.array([correspoints])
-
-
-def H_matrix(m,w1,w2,h, shape="L", I0=1):
-    def esc_linear(m,h,w, xe ,xm):
-        d_2 = math.sqrt(1/(m+3))*h
-        d_3 = math.sqrt(3/(m+3))*h
-
-        if d_2 < xm and d_3 < xm:
-            raise ValueError("check:\n d_2 = {d_2}, xm = {xm}\n d_3 = {d_3}, xm = {xm}")
-            
-        n =2
-        L  = esc_array(m,h,n,M=1,shape="L",approx=False,half=True)
-        Lw = L
-        Lw = L
-        Lw = L
-        while Lw[2][0]/2 < xe:
-            print("{}{}".format(Lw[1]/2,xe))
-            L = Lw
-            n += 1
-            Lw = esc_array(m,h,n,M=1,shape="L",approx=False,half=True)
-        return L[3]
-
-
-    xex = xe(h,w1,m)
-    xmx = xm(h,w2,m,xex)
-    xarr = esc_linear(m,h,w1, xex, xmx)
-    xarray = find_corres_BC(xarr, xex,xmx,h,w1,m)
-    
-    if shape == "R":
-        xey = xe(h,w1,m)
-        xmy = xm(h,w2,m,xey)
-        yarr = esc_linear(m,h,w2, xey, xmy)
-        yarray = find_corres_BC(yarr, xey,xmy,h,w2,m)
-        return LEDmatrix(m,I0,xarray,yarray)
-
-    return LEDmatrix(m,I0,xarray,np.array([[0]]))
-   
-
-'''
-def generate_LEDmatrix(m,h,N,M=1, method="hyeon"):
-
-    if method == "hyeon":
-        pass
-    elif method == "morenaL":
-        pass
-    elif method == "morenaR"
-
-    return LEDmatrix
-'''
+       
