b0ab0ed6681896f191f9253ab6dd247b38ac74c5
Kim, Hyunseong <qwqwhsnote@gm.gist.ac.kr>
2023-03-27 16:59:19 +0900

    Cheb2 function utils finished

--- uilc/utils/cheb2.py
+++ uilc/utils/cheb2.py
@@ -1,183 +1,35 @@
-import math
-
+# Python mild imeplementation of Chebfun library 2d approximation.
+# Original project: https://www.chebfun.org/
+# Algorithm details: Townsend, A., & Trefethen, L. N. (2013). An extension of Chebfun to two dimensions. SIAM Journal on Scientific Computing, 35(6), C495-C518.
 from typing import Callable, Tuple, Union
-from numpy.typing import NDArray, ArrayLike
 from numbers import Number
 from collections.abc import Iterable
 
+
 import numpy as np
+from numpy.polynomial import Chebyshev
+from numpy.typing import NDArray
 
-from numpy.polynomial.chebyshev import Chebyshev
-from scipy.optimize import bisect
-
-EPS = 10E6*np.finfo(float).eps
-
-# Discrete version
-def pmf2cdf(prob_mass):
-    n = prob_mass.shape[0]
-    result = np.zeros(n)
-    for i, e in enumerate(prob_mass):
-        result += np.concatenate([np.zeros(i), e*np.ones(n-i)])
-    #result -= result.min()
-    return result
-def pmf2cdf_2d(prob_mass):
-    n, m = prob_mass.shape
-    result = np.zeros(shape=(n, m))
-    for i in range(0, n):
-        for j in range(0,m):
-            row = np.zeros(shape=(i, m)) if i != 0 else None
-            col = np.zeros(shape=(n-i, j)) if j !=0 else None
-
-            add_mass = prob_mass[i,j] * np.ones(shape=(n-i, m-j))
-
-            if col is not None:
-                add_mass = np.concatenate([col, add_mass], axis=1)
-                
-            if row is not None:
-                add_mass = np.concatenate([row, add_mass], axis=0)
-            
-            result += add_mass
-    result -= result.min()
-    return result
-
-def pmf_cond(i, pro_mass, axis =0):# axis=0: x, axis=1: y
-    n,m = pro_mass.shape
-    if axis==0:
-        if not(i>=0 and i<n):
-            raise IndexError("Exceeded data dimension.")
-        pdf = pro_mass[i]
-    elif axis ==1:
-        if not(i>=0 and i<m):
-            raise IndexError("Exceeded data dimension.")
-        pdf = pro_mass[:, i]
-    
-    return pdf /pdf.sum()
-
-def cdf_cond(i, pro_mass, axis=0):# axis=0: x, axis=1: y
-    return pmf2cdf(pmf_cond(i, pro_mass, axis))
-
-
-# Chebyshev approximation
-def get_cheb_approx_pdf(pos_mass, x, region=(None, None)):
-    if pos_mass.shape != x.shape:
-        raise ValueError("Dimensions are not same each others.")
-    
-def get_cheb_approx_cdf_pdf(pos_mass, x, region=(None, None)):
-    if pos_mass.shape != x.shape:
-        raise ValueError("Dimensions are not same each others.")    
-    
-def get_cheb_approx_cdf_pmf(pos_mass, dx, region=(None, None)):
-    return 0
-
-
-# Inverse Transform Sampling
-
-# Direct
-def int_sampling(
-        uni_sam, 
-        pmf= None,
-        cdf = None,
-        domain = None,
-        interpolate = False # 1 dim interpolate between sample points
-        ):
-    if pmf.min() <0 or pmf.max() >1:
-        raise ValueError("Invaild value in probability mass vector. Exceeding range [0,1]")
-    if math.fabs(pmf.sum() -1) > EPS:
-        raise ValueError("Invaild probability mass vector. The sum is not 1.")
-
-    if uni_sam.min() <0 or uni_sam.max() >1:
-        raise ValueError("Invaild value in sample data. Exceeding range [0,1]")
-    if pmf is None and cdf is None:
-        raise ValueError("")
-    
-    if isinstance(domain, Iterable) and len(domain) ==2:
-        domain = np.linspace(domain[0], domain[1], endpoint=True)
-
-    if cdf is None:
-        cdf = pmf2cdf(pmf)
-    
-    if domain is not None and domain.size != cdf.size:
-        raise ValueError("Dimension error, domain and cdf have different dimension.")
-        
-    n = cdf.size
-
-    if interpolate:
-        samples = []
-        for uni_i in uni_sam:
-            if uni_i <= cdf.min():
-                samples.append(0)
-                continue
-            if uni_i >= cdf.max():
-                samples.append(n-1)
-                continue
-            
-            min_sol = np.where(cdf <=uni_i)[0]
-            max_sol = np.where(cdf >=uni_i)[0]
-
-
-            min_index = min_sol.max() if min_sol.size != 0 else None
-            max_index = max_sol.min() if max_sol.size != 0 else None
-
-            if min_index == max_index:
-                if min_index is None:
-                    pass # impossible
-                else:
-                    samples.append(min_index)
-            elif min_index is None or max_index is None:
-                if min_index is None:
-                    samples.append(max_index)
-                else:
-                    samples.append(min_index)
-            else:
-                dy = cdf[max_index] - cdf[min_index]
-                dy_i = uni_i - cdf[min_index]
-                dx_i = dy_i / dy
-                samples.append(min_index + dx_i)
-        result = np.array(samples)
-    else:
-        results = []
-        uni_sam.sort()
-
-        for i, c_i in enumerate(cdf):
-            uni_index = np.where(uni_sam <= c_i)[0]
-            if uni_index.size == 0:
-                continue
-            results.append(i*np.ones(uni_index.size))
-            uni_sam = uni_sam[uni_index.size:]
-
-        if domain is not None:
-            for i in range(0, len(results)):
-                results[i] *= domain[i]
-
-        result = np.concatenate(results)
-        
-    return result
-
-# Continuous version
-## Root finding
-def int_sampling_root(
-        uni_sam,
-        cdf:Callable,
-        domain
-    ):
-    a, b = domain
-    if a > b:
-        a, b = b, a
-
-    cdf = lambda x: cdf()
-    result = []
-    for uni_s in uni_sam:
-        result.append(bisect(lambda x: cdf(x) - uni_s, *domain))
-    return np.array(result)
-
-# SVD Chebyshev approximation
-# Reference Cheb2fun 2d 
 SingleFunction = Callable[[Number], Number]
 Callable_check = np.frompyfunc(callable, 1, 1)
-def Cheby_stage_1_params(i):
+
+# Basic utils
+def cheby_range_transform(x, a, b): # [-1, 1] -> [a, b]
+    return ((b-a)/2)*x + (b+a)/2
+def cheby_range_inv_transform(x, a, b): # [a, b] -> [-1, 1]
+    return (2/(b-a))*x - (b+a)/(b-a)
+def cheby_ext_grid(a, b, n):
+    x_i = np.cos((np.pi/n-1)*np.arange(n))
+    return cheby_range_transform(x_i, a, b)
+def cheby_root_grid(a, b, n):
+    x_i = np.cos((np.pi/n)*(0.5+np.arange(n)))
+    return cheby_range_transform(x_i, a, b)
+
+
+def _cheby_stage_1_params(i):
     i +=1
     return int(2**(i+2)+1), int(2**(i)+1)
-def get_xy_decompose(
+def get_xy_cheby_decompose(
         f:Callable, 
         point:Tuple[Number, Number], 
         domain:Tuple[Number, Number, Number, Number], 
@@ -199,17 +51,18 @@ def get_xy_decompose(
     u_i = Chebyshev.fit(uy_list, uz_list, deg=cheby_deg, domain=(b, d))
     return v_i, u_i
 
+
 class RankApprox2dim:
     def __init__(self, 
                  fx_list:Union[SingleFunction, Iterable[SingleFunction]], 
                  fy_list:Union[SingleFunction, Iterable[SingleFunction]], 
                  weights:Union[Number, Iterable[Number]], 
-                 weight_dtype = float,
+                 w_domain = float,
                  domain:Union[None, Tuple[Number, Number, Number, Number]]=None, 
                  **kwargs):
         self._fx_list = np.array(fx_list) if not isinstance(fx_list, np.ndarray) else fx_list
         self._fy_list = np.array(fy_list) if not isinstance(fy_list, np.ndarray) else fy_list
-        self._weights = np.array(weights, dtype=weight_dtype) if not isinstance(weights, np.ndarray) else weights
+        self._weights = np.array(weights, dtype=w_domain) if not isinstance(weights, np.ndarray) else weights
 
         if (self._fx_list.shape) != 1:
             self._fx_list = self._fx_list.reshape(-1) 
@@ -289,7 +142,7 @@ class RankApprox2dim:
             raise TypeError("fy list must consist of callable objects.")
         if  not np.issubdtype(weights.dtype, np.number):
             raise TypeError("Weight must consist of number type.")
-
+    #-----------------------------------------------------------------------------
     @classmethod
     def from_function_approx(cls, 
                              f:Callable, 
@@ -307,7 +160,7 @@ class RankApprox2dim:
         
         err = 1.0
         for j in range(initial_step, max_step):
-            n, max_step_stage_1 = Cheby_stage_1_params(j)
+            n, max_step_stage_1 = _cheby_stage_1_params(j)
 
             cheby_deg= (n-1)
 
@@ -345,10 +198,11 @@ class RankApprox2dim:
                     break
 
         return f_k, err
+    #-----------------------------------------------------------------------------
     def rank_up(self, 
-                        fx_list:Union[SingleFunction, Iterable[SingleFunction]], 
-                        fy_list:Union[SingleFunction, Iterable[SingleFunction]], 
-                        weights:Union[Number, Iterable[Number]]):
+                fx_list:Union[SingleFunction, Iterable[SingleFunction]], 
+                fy_list:Union[SingleFunction, Iterable[SingleFunction]], 
+                weights:Union[Number, Iterable[Number]]):
         
         fx_list = list(fx_list) if isinstance(fx_list, Iterable) and hasattr(fx_list, "__getitem__") else [fx_list]
         fy_list = list(fy_list) if isinstance(fy_list, Iterable) and hasattr(fy_list, "__getitem__") else [fy_list]
@@ -369,15 +223,20 @@ class RankApprox2dim:
     def rank_down(self, a:None, b:None):
         if a is None and b is None:
             # del last element
+            self._fx_list = np.delete(self._fx_list[: -a], np.s_[-1])
+            self._fy_list = np.delete(self._fy_list[: -a], np.s_[-1])
+            self._weights = np.delete(self._weights[:-a] , np.s_[-1])
             pass
         elif b is None:
             # del last 'a' length elements
-            pass
+            self._fx_list = np.delete(self._fx_list, np.s_[-a:])
+            self._fy_list = np.delete(self._fy_list, np.s_[-a:])
+            self._weights = np.delete(self._weights, np.s_[-a:])
         else:
             # del from a to b elements
-            pass
-
-
+            self._fx_list = np.delete(self._fx_list, np.s_[a:b])
+            self._fy_list = np.delete(self._fy_list, np.s_[a:b])
+            self._weights = np.delete(self._weights, np.s_[a:b])
     #--------------------------------------------------------------------------------
     def elements(self, a:int, b:Tuple[None, int] = None):
         if b is None: 
@@ -450,60 +309,3 @@ class RankApprox2dim:
     @property
     def rank(self):
         return self._weights.size
-
-# Chebyshev
-from numpy.polynomial.chebyshev import Chebyshev
-def cheby_range_transform(x, a, b): # [-1, 1] -> [a, b]
-    return ((b-a)/2)*x + (b+a)/2
-def cheby_range_inv_transform(x, a, b): # [a, b] -> [-1, 1]
-    return (2/(b-a))*x - (b+a)/(b-a)
-def cheby_ext_grid(a, b, n):
-    x_i = np.cos((np.pi/n-1)*np.arange(n))
-    return cheby_range_transform(x_i, a, b)
-def cheby_root_grid(a, b, n):
-    x_i = np.cos((np.pi/n)*(0.5+np.arange(n)))
-    return cheby_range_transform(x_i, a, b)
-#--------------------------------------------------
-def function_vec(f_arr, x, multi=False): # Array of functions apply to x value
-    if multi:
-        return np.fromiter((fi(*x) for fi in f_arr), dtype=np.dtype(type(x)))
-    else:
-        return np.fromiter((fi(x) for fi in f_arr), dtype=np.dtype(type(x)))
-def cal_f_xy_approx(x, y, f_x, f_y, coefs):
-    f_x_v = function_vec(f_x, x)
-    f_y_v = function_vec(f_y, y)
-    return coefs.dot(f_x_v.dot(f_y_v))
-f_xy_approx_vec = np.vectorize(cal_f_xy_approx, excluded=["f_x", "f_y", "coefs"])
-
-def u(x, f, domain, deg): # return u(y)=f(x_i, y) Chebyshev approximation
-    a, b, c, d = domain
-    
-    y_list = np.linspace(b, d, deg+1, endpoint=True)
-    x_list = np.full_like(y_list, x)
-    z_list = f(np.hstack(x_list, y_list))
-    u_i = Chebyshev.fit(y_list, z_list, deg=deg, domain=domain)
-    return u_i
-def v(y, f, domain, deg):# return v(x)= f(x, y_i) Chebyshev approximation
-    a, b, c, d = domain
-    
-    x_list = np.linspace(a, c, deg+1, endpoint=True)
-    y_list = np.full_like(x_list, y)
-    z_list = f(np.hstack(x_list, y_list))
-    v_i = Chebyshev.fit(x_list, z_list, deg=deg, domain=domain)
-    return v_i
-def u_i(xy_list, index, f, domain, deg):
-    x, y = xy_list[index]
-    return u(x, f, domain, deg)
-def v_i(xy_list, index, f, domain, deg):
-    x, y = xy_list[index]
-    return v(y, f, domain, deg)
-def ge_approx(xy_points, f, domain, chebyshev_deg=100):
-    z = f(*xy_points.transpose())
-    max_i = np.argmax(z)
-    u_func = [u_i(xy_points, max_i, f, domain, chebyshev_deg)]
-    v_func = [v_i(xy_points, max_i, f, domain, chebyshev_deg)]
-    d_coef = [1/(f(*xy_points[max_i]))]
-    for point in xy_points:
-        pass
-
-
